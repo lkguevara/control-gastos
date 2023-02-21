ce01c0fbda49c149e8b3c26b14c8ee5cdd85d82c
Lian Katherin Guevara <liann.guevara@gmail.com>
2023-02-20 22:31:10 -0500

    create components Header - Budget

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Presupuesto from './Presupuesto'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>PLANIFICADOR DE GASTOS</h1>
+        <Presupuesto />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Presupuesto.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Presupuesto = () => {
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+        <form className='formulario'>
+            <div className="campo">
+                <label>Definir presupuesto</label>
+                <input 
+                    className='nuevo-presupuesto'
+                    type="text" 
+                    placeholder= "Añade tu presupuesto"
+         
+                />
+            </div>
+
+            <input 
+                type = "submit" 
+                value = "Añadir"
+            />
+        </form>
+    </div>
+    
+  )
+}
+
+export default Presupuesto
